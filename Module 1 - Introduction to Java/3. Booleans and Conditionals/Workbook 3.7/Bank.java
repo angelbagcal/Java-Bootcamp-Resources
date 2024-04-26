63e89110fae428aa2dd7abd8739a589b4ea3e4c1
angelbagcal <167283841+angelbagcal@users.noreply.github.com>
2024-04-27 00:29:34 +1000

    Update Bank.java

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.7/Bank.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.7/Bank.java
@@ -8,29 +8,30 @@ public class Bank {
         Scanner scan = new Scanner(System.in);
         System.out.println("\n****ROYAL BANK OF JAVA****");
         System.out.println("Are you here to get a mortgage? (yes or no)");
-        //Task 1 - Pick up the user's decision.
-
-
-        //Task 2 - Print this if the decision is "yes"
-        System.out.println("\nGreat! In one line" +
+        
+        String userInput = scan.nextLine();
+        
+        switch (userInput) {
+            case "yes" :  System.out.println("\nGreat! In one line" +
             "\nHow much money do you have in your savings?" +
             "\nAnd, how much do you owe in credit card debt?");
-                // Task 3 - Pick up each value 
+            int savings = scan.nextInt();
+            int debt = scan.nextInt();
+            
             System.out.println("\nHow many years have you worked for?");
-               // Task 4 - Pick up number of years
-
+            int yearsWorked = scan.nextInt();
+            
             System.out.println("What is your name?");
-               // Task 5 - Pick up the user's name
-                
-               //Task 6 - Approve the mortgage if they meet the requirements (see article)
-               //       - Otherwise, don't give them a mortgage.          
-               System.out.println("Congratulations <name> You have been approved!");
-               System.out.println("Sorry, you are not eligible for a mortgage.");
-
-        //Task 2 - Print this if the decision was not "yes"
-        System.out.println("\nOK. Have a nice day!");
-
-
+            String name = scan.next();
+            
+            if (savings >= 1000 && debt < 5000 && yearsWorked > 2) {
+                System.out.println("Sorry, you are not eligible for a mortgage.");
+            } else {
+                System.out.printf("Congratulations %s You have been approved!", name);
+            } break;
+            case "no" :   System.out.println("\nOK. Have a nice day!");
+        }
+      
         scan.close();
     }
 }
