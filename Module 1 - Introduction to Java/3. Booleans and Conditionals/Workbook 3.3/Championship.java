40dd916caa00e40812c53bd45d4afbd63e6a975f
angelbagcal <167283841+angelbagcal@users.noreply.github.com>
2024-04-26 23:06:18 +1000

    Update Championship.java

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
@@ -5,7 +5,16 @@ public class Championship {
 
         int gryffindor = 400;    //gryffindor points
         int ravenclaw = 200;    //ravenclaw points
-
+        
+        if (gryffindor > (ravenclaw+300)) {
+            System.out.println("Gryffindor wins");
+        } else if (gryffindor >= ravenclaw) {
+            System.out.println("Gryffindor places 2nd");
+        } else if (gryffindor <= (ravenclaw-100)) {
+            System.out.println("Gryffindor places 3rd");
+        } else if (gryffindor < (ravenclaw-100)) {
+            System.out.println("Gryffindor places 4th");
+        }
         // int margin = amount of points by which gryffindor scored over ravenclaw;
         
     }
