cdc4f63c27c11da3f6456b1bd6c7034037cef04b
angelbagcal <167283841+angelbagcal@users.noreply.github.com>
2024-04-26 19:13:35 +1000

    Update Chatbot.java

--- Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
+++ Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
@@ -1,4 +1,4 @@
-//import Scanner
+import java.util.Scanner;
 
 public class Chatbot {
     public static void main(String[] args) {
@@ -7,20 +7,26 @@ public class Chatbot {
 
         
         //set up scanner. 
+        Scanner scnr = new Scanner;
 
         System.out.println("Hello. What is your name?");
         //Pick up user's name and store it. 
+        String userName = scnr.nextLine();
 
                            
         //add new a line before asking next question. 
         System.out.println("Hi <name>! I'm Javabot. Where are you from?");
         //Pick up user's home and store it. 
+        String userHome = scnr.nextLine;
+        System.out.println();
                         
 
         //add new a line before asking next question.
         System.out.println("I hear it's beautiful at <home>! I'm from a place called Oracle");
         System.out.println("How old are you?");
         //Pick up age and store it.
+        int userAge = scnr.nextInt;
+        System.out.println();
 
 
         //add new a line before asking next question.
@@ -28,11 +34,13 @@ public class Chatbot {
         System.out.println("This means I'm <divide 400 by age> times older than you.");
         System.out.println("Enough about me. What's your favourite language? (just don't say Python)");
         //Pick up language and store it. 
-
+        String language = scnr.next();
+        System.out.println();
 
         //add new a line here.
         System.out.println("<language>, that's great! Nice chatting with you <name>. I have to log off now. See ya!");
-        
+
+        scnr.close();
         //close scanner. 
 
         
