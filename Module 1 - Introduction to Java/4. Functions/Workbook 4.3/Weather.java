b5fe8ae32ff2fe87c934edef3d5eb7e49a7f4c0d
angelbagcal <167283841+angelbagcal@users.noreply.github.com>
2024-04-28 14:03:04 +1000

    Update Weather.java

--- Module 1 - Introduction to Java/4. Functions/Workbook 4.3/Weather.java
+++ Module 1 - Introduction to Java/4. Functions/Workbook 4.3/Weather.java
@@ -1,35 +1,24 @@
 public class Weather {
     public static void main(String[] args) {
-        double noon = 77;          //temperature at noon in fahrenheit.
-        double evening = 61;       //temperature during the evening in fahrenheit
-        double midnight = 55;      //temperature at midnight in fahrenheit
-
-        //Task 3 - Call the printTemperatures function. 
+        double noon = 77.0;          //temperature at noon in fahrenheit.
+        double evening = 61.0;       //temperature during the evening in fahrenheit
+        double midnight = 55.0;      //temperature at midnight in fahrenheit
         
+        printTemperatures(noon);
+        printTemperatures(evening);
+        printTemperatures(midnight);
         
     }
 
-
-    //Task 1: Make a function here. See the doc comments below. 
-
-    /**
-     * Function name: fahrenheitToCelsius - converts fahrenheit to celcius
-     * @param fahrenheit (double)
-     * @return celsius (double)
-     * 
-     * Inside the function:
-     * 1. returns the temperature in celcius. C = (F - 32) * 5/9. 
-     */
-
-     
-    //Task 2: Make a function here. See the doc comments below. 
-
-    /**
-     * Function name: printTemperatures – prints both temperature values. 
-     * @param fahrenheit (double)
-     * 
-     * Inside the function: 
-     *  1. prints: F: <temperature in fahrenheit>.
-     *  2. prints: C: <temperature in celsius> . 
-     */
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32.0)*5.0/9.0;
+        
+    }
+    
+    public static void printTemperatures(double fahrenheit) {
+        double celsius = fahrenheitToCelsius(fahrenheit);
+        System.out.println("F: " + String.format("%.1f", fahrenheit));
+        System.out.println("C: " + String.format("%.1f",celsius));
+        System.out.println();
+    }
 }
