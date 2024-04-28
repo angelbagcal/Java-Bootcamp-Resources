1a3ba95e701107e7df83ce9bd9378fecaf016818
angelbagcal <167283841+angelbagcal@users.noreply.github.com>
2024-04-28 15:54:19 +1000

    Update Megaphone.java

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.1/Megaphone.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.1/Megaphone.java
@@ -1,6 +1,10 @@
 public class Megaphone {
     public static void main(String[] args) {
-        // See Learn the Part for the instructions.
+    String statement = "\t if Java was easy, they would call it Python!";
+
+    for (int i=1 ; i < 10 ; i++) {
+        System.out.printf(i + ". " + statement + "\n");
+        }
     }
     
 }
