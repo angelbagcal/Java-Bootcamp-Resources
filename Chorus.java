e10a8e5155559f5aab84a159aa4f0c7dd22af2ca
angelbagcal <167283841+angelbagcal@users.noreply.github.com>
2024-04-28 12:25:01 +1000

    Update Chorus.java

--- Chorus.java
+++ Chorus.java
@@ -1,10 +1,19 @@
 public class Chorus {
+    
     public static void main(String[] args) {
         // Chorus needs to be sung 5 times
+        int amount = 5;
+        
+        for (int i = 0 ; i < amount ; i++) {
+        singChorus();
+        }
+
+    }
+    
+    public static void singChorus() {
         System.out.println("Don't blame it on the sunshine");
         System.out.println("Don't blame it on the moonlight");
         System.out.println("Don't blame it on good times");
         System.out.println("Don't blame it on the boogie\n");
-
     }
 }
