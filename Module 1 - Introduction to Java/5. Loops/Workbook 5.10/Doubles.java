a2667814c0181d1b859372a1a0d0feaa4b3b6ca7
angelbagcal <167283841+angelbagcal@users.noreply.github.com>
2024-04-29 17:43:26 +1000

    Update Doubles.java

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.10/Doubles.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.10/Doubles.java
@@ -1,12 +1,26 @@
+import java.util.Random;
+
 public class Doubles {
     public static void main(String[] args) {
-
-   //      See Learn the Part for instructions.
        
-        int dice1;
-        int dice2;
-
-
+        int dice1 = rollDice();
+        int dice2 = rollDice();
+    
+    while (!(dice1 == dice2)) {
+    dice1 = rollDice();
+    System.out.println("Dice 1: " + dice1);
+    dice2 = rollDice();
+    System.out.println("Dice 2: " + dice2 + "\n");
+        }
+    
+    System.out.println("Dice 1: " + dice1);
+    System.out.println("Dice 2: " + dice2+ "\n");
+    System.out.println("You rolled doubles!");    
+    }
+    
+    public static int rollDice() {
+        Random random = new Random();
+        return random.nextInt(6)+1;
     }
 
 }
